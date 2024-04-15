9d7000414b7638c64148de2127a072f9490ef7ee
Thinley <thinleylama44@gmail.com>
2024-04-15 18:19:48 +0530

    css

--- packages/ui/src/TextInput.tsx
+++ packages/ui/src/TextInput.tsx
@@ -11,6 +11,6 @@ export const TextInput = ({
 }) => {
     return <div className="pt-2">
         <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-        <input onChange={(e) => onChange(e.target.value)} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} />
+        <input onChange={(e) => onChange(e.target.value)} type="number" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} />
     </div>
 }
