3b608a89cb27b53f736e825c776d3dbe7e84bde2
Abhijit-Jha <abhijeetjha913@gmail.com>
2024-09-14 14:26:09 +0530

    Changes 2

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server"
 import   client   from "@repo/db/client";
 
-
+// const client = new PrismaClient();
 
 export const GET = async () => {
-    
+    // await client.user.create({
+    //     data: {
+    //         email: "asd",
+    //         name: "adsads"
+    //     }
+    // })
     return NextResponse.json({
         message: "hi there"
     })
